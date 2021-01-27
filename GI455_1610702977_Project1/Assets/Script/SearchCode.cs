5165930f78e9199db6b77fefff20072b2291c2a8
DESKTOP-JMOO8VF\User <Holiness222@gmail.com>
2021-01-27 20:07:51 +0700

    Send Assignment Week1

---
+++ GI455_1610702977_Project1/Assets/Script/SearchCode.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SearchCode : MonoBehaviour
+{
+    public string Guitar, Computer, Book, Fan, Money;
+    public Text inputField;
+    public Text textDisplay;
+    
+    public void resultName()
+    {
+        if ( inputField.text == "Guitar")
+        {
+            
+            Guitar = inputField.GetComponent<Text>().text;
+            textDisplay.GetComponent<Text>().text = "[ " + "<color=green>Guitar</color>" + " ] is found.";         
+        }
+        else if ( inputField.text == "Book")
+        {
+            
+            Book = inputField.GetComponent<Text>().text;
+            textDisplay.GetComponent<Text>().text = "[ " + "<color=green>Book</color>" + " ] is found.";
+
+        }
+        else if ( inputField.text == "Computer")
+        {
+            
+            Computer = inputField.GetComponent<Text>().text;
+            textDisplay.GetComponent<Text>().text = "[ " + "<color=green>Computer</color>" + " ] is found.";
+            
+        }
+        else if ( inputField.text == "Fan" )
+        {
+            
+            Fan = inputField.GetComponent<Text>().text;
+            textDisplay.GetComponent<Text>().text = "[ " + "<color=green>Fan</color>" + " ] is found.";
+            
+        }
+        else if ( inputField.text == "Money" )
+        {
+            
+            Money = inputField.GetComponent<Text>().text;
+            textDisplay.GetComponent<Text>().text = "[ " + "<color=green>Money</color>" + " ] is found.";
+        }
+        else
+        {
+            
+            textDisplay.GetComponent<Text>().text = " [] is not found. ";
+        }
+    }
+}
