dba51f70db9dfcb1fd98ecdb7c6adfee032e86f2
DESKTOP-JMOO8VF\User <Holiness222@gmail.com>
2021-02-04 03:42:48 +0700

    Send Assigment Week2

---
+++ GI455_1610702977_Project1/GI455 W.2/Week 2/Assets/ProgramChat/Script/ChatSystem.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatSystem : MonoBehaviour
+{
+
+    public string username;
+    public int maxMessaages = 25;
+
+    public GameObject chatpanel, textdisplay;
+    public InputField chatBox;
+
+    public Color playerMessage, info;
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+  
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(chatBox.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+
+                SendMessageToChat(username + ": " + chatBox.text,Message.MessageType.playerMessage);
+                chatBox.text = "";
+
+            }
+        }
+        else
+        {
+
+            if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+                chatBox.ActivateInputField();
+
+        }
+
+        if (chatBox.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                //SendMessageToChat("You pressed the space bar!",Message.MessageType.info);
+                Debug.Log("Space");
+            }
+
+
+        }    
+    }
+
+    public void SendMessageToChat(string text, Message.MessageType messageType)
+    {
+        if (messageList.Count >= maxMessaages)
+        {
+            Destroy(messageList[0].textdisplay.gameObject);
+            messageList.Remove(messageList[0]);
+
+        }
+        
+
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textdisplay, chatpanel.transform);
+
+        newMessage.textdisplay = newText.GetComponent<Text>();
+
+        newMessage.textdisplay.text = newMessage.text;
+        newMessage.textdisplay.color = MessageTypeColor(messageType);
+
+        messageList.Add(newMessage);
+
+    }
+
+    Color MessageTypeColor(Message.MessageType messageType)
+    {
+
+        Color color = info;
+
+        switch (messageType)
+        {
+            case Message.MessageType.playerMessage:
+                color = playerMessage;
+                break;
+         
+        }
+
+
+            return color;
+
+    }
+
+
+    [System.Serializable]
+    public class Message
+    {
+
+        public string text;
+        public Text textdisplay;
+
+        public enum MessageType
+        {
+
+            playerMessage,info
+
+        }
+
+    }
+}
---
+++ GI455_1610702977_Project1/GI455 W.2/Week 2/Assets/ProgramChat/Script/Websocketconnection.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.UI;
+
+namespace ProgramChat
+{
+    public class Websocketconnection : MonoBehaviour
+    {
+
+        private WebSocket websocket;
+
+        public InputField ip;
+        public InputField port;
+
+        public Text textDisplay1, textDisplay2,textMessage;
+        public InputField chatBox;
+
+        void Start()
+        {
+            
+            
+            
+        }
+        void Update()
+        {
+
+            
+                
+        }
+        public void openSever()
+        {
+
+            if (ip.text == "127.0.0.1"||port.text == "5500")
+            {
+
+                websocket = new WebSocket("ws://127.0.0.1:5500/");
+
+                websocket.OnMessage += OnMessage;
+
+                websocket.Connect();
+
+                textMessage.GetComponent<Text>().text = "Successfully connected";
+            }
+
+            else
+            {
+
+                textMessage.GetComponent<Text>().text = "Failed to connect";
+
+            }
+
+        }
+        public void chatDisplay()
+        {
+            if (websocket.ReadyState == WebSocketState.Open)
+
+            {
+                websocket.OnMessage += OnMessage;
+                websocket.Send(chatBox.text);
+                //textDisplay1.GetComponent<Text>().text = "Malila : " + chatBox.text;
+                //chatBox.text = "";
+            }
+          
+        }
+        private void OnDestroy()
+        {
+            if (websocket != null)
+            {
+                websocket.Close();
+            }
+        }
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Debug.Log("Receive msg : " + messageEventArgs.Data);
+
+            if (messageEventArgs.Data == chatBox.text)
+            {
+
+                textDisplay1.text = messageEventArgs.Data;
+
+            }
+
+            else
+            {
+
+                textDisplay2.text = messageEventArgs.Data;
+
+            }                              
+        }
+    }    
+}
+
+
